e232d87465303ecdc8043501ac51e6ddcea6ddee
J-I-H-O <jeongjiho0731@gmail.com>
2023-11-12 11:58:30 +0900

    feat(VisitingDate): 올바른 값인지 검증하는 로직 추가

--- src/main/java/christmas/model/VisitingDate.java
+++ src/main/java/christmas/model/VisitingDate.java
@@ -1,16 +1,28 @@
 package christmas.model;
 
 public class VisitingDate {
+    private static final String NOT_NUMERIC_ERROR_MESSAGE = "[ERROR] 숫자만 입력할 수 있습니다.";
     private static final String INVALID_DATE_ERROR_MESSAGE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
 
     private final int date;
 
-    public VisitingDate(int date) {
-        validateDate(date);
+    public VisitingDate(String dateInput) {
+        validateNumeric(dateInput);
+        int date = Integer.parseInt(dateInput);
+        validateDateRange(date);
+
         this.date = date;
     }
 
-    private void validateDate(int date) {
+    private void validateNumeric(String dateInput) {
+        try {
+            Integer.parseInt(dateInput);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(NOT_NUMERIC_ERROR_MESSAGE);
+        }
+    }
+
+    private void validateDateRange(int date) {
         if (!(1 <= date && date <= 31)) {
             throw new IllegalArgumentException(INVALID_DATE_ERROR_MESSAGE);
         }
