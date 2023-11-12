d28cf86f1d10bf92e78bdfda7dfcf63956b4a985
J-I-H-O <jeongjiho0731@gmail.com>
2023-11-12 14:23:27 +0900

    feat(Quantity): 메뉴 개수 검증 로직 작성

--- src/main/java/christmas/model/menu/Quantity.java
+++ src/main/java/christmas/model/menu/Quantity.java
@@ -1,14 +1,34 @@
 package christmas.model.menu;
 
 public class Quantity {
+    private static final String INVALID_INPUT_ERROR_MESSAGE = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
+    private static final int MIN_QUANTITY = 1;
+
     private final int quantity;
 
-    public Quantity(String quantity) {
-        // TODO: 검증 로직
-        this.quantity = 0;
+    public Quantity(String quantityInput) {
+        validateNumeric(quantityInput);
+        int quantity = Integer.parseInt(quantityInput);
+        validateQuantityRange(quantity);
+
+        this.quantity = quantity;
     }
 
     public int getQuantity() {
         return quantity;
+    }
+
+    private void validateNumeric(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INVALID_INPUT_ERROR_MESSAGE);
+        }
+    }
+
+    private void validateQuantityRange(int quantity) {
+        if (quantity < MIN_QUANTITY) {
+            throw new IllegalArgumentException(INVALID_INPUT_ERROR_MESSAGE);
+        }
     }
 }
