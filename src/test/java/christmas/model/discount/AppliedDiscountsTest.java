9d501e8d65d88c35aa18b40b0fc926c8289866f9
J-I-H-O <jeongjiho0731@gmail.com>
2023-11-15 22:00:55 +0900

    test(AppliedDiscountsTest): 총주문 금액 만원 미만 시 이벤트 참여 불가  테스트 추가

--- src/test/java/christmas/model/discount/AppliedDiscountsTest.java
+++ src/test/java/christmas/model/discount/AppliedDiscountsTest.java
@@ -1,7 +1,22 @@
 package christmas.model.discount;
 
-import static org.junit.jupiter.api.Assertions.*;
+import christmas.model.common.VisitingDate;
+import christmas.model.menu.OrderMenus;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 class AppliedDiscountsTest {
+    @Test
+    void 총주문_금액이_10000원_미만이면_이벤트_참여불가() {
+        String dateInput = "5";
+        String orderMenusInput = "아이스크림-1";
 
+        VisitingDate visitingDate = new VisitingDate(dateInput);
+        OrderMenus orderMenus = new OrderMenus(orderMenusInput);
+        AppliedDiscounts appliedDiscounts = new AppliedDiscounts(visitingDate, orderMenus);
+
+        int expected = 0;
+        int benefitAmount = appliedDiscounts.calculateTotalBenefitAmount();
+        Assertions.assertEquals(expected, benefitAmount);
+    }
 }
